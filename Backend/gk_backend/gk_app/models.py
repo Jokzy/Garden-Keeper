a862975e6624ebfae9f66327feb0c297dc1df198
CamMark <camimarquis@gmail.com>
2024-03-01 12:47:53 -0500

    Rajouter les modules models, serializers et views

--- Backend/gk_backend/gk_app/models.py
+++ Backend/gk_backend/gk_app/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Plante(models.Model):
     nom = models.CharField(max_length=255)
     nom_scientifique = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nom
--- Backend/gk_backend/gk_app/views.py
+++ Backend/gk_backend/gk_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import PlanteSerializer
+from .models import Plante
+
+
 # Create your views here.
+class PlanteView(viewsets.ModelViewSet):
+    serializer_class = PlanteSerializer
+    queryset = Plante.objects.all()
