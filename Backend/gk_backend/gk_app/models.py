a8389d260df19ace40d38cfda7c861d7f9bb2c9c
CamMark <camimarquis@gmail.com>
2024-03-01 12:17:18 -0500

    Changer la directory des affaires.

---
+++ Backend/gk_backend/gk_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# Create your models here.
+class Plante(models.Model):
+    nom = models.CharField(max_length=255)
+    nom_scientifique = models.CharField(max_length=255)
