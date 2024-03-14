0919efb8b6ade8c78d969628fee29163ac442b9a
Sonia Ly <lysoksonia@gmail.com>
2024-03-14 13:16:09 -0400

    Just-in-case push

--- Backend/gk_backend/gk_backend/urls.py
+++ Backend/gk_backend/gk_backend/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from gk_app import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'plante', views.PlanteView, 'plante')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
