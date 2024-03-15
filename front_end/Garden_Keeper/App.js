f9caed7874a3df831d672ebcb042c065a2793432
Jokzy <joeelhachem05@gmail.com>
2024-03-15 07:57:27 -0400

    jdjdj
    hhhd

--- front_end/Garden_Keeper/App.js
+++ front_end/Garden_Keeper/App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {Button, Linking, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const [name, setName]  = useState(  'Style Test')
+const onClickHandler = () => {
+setName('Style Test is Done!')
+
 }
+  return (
+
+
+    <View style={styles.container}>
+      <Text style={ styles.text}> {name}  </Text>
+      <Button style={styles.text} title= {'Update'} onPress={ onClickHandler   } >
+
+      </Button>
+    </View>
+
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+    width: '100%',
+    height : '50%',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth : 10 ,
+    borderColor: '#f03',
+    borderRadius : 10,
+    borderTopColor : 'black',
+    borderBottomColor : 'black'
   },
+  text:{
+    color: '#ff00ff',
+    alignItems :'center',
+    justifyContent:'center',
+    fontStyle : "italic",
+    fontFamily : "Times New Roman",
+    fontWeight : "bold",
+    fontSize : 30,
+
+  },
+
+
+
 });
