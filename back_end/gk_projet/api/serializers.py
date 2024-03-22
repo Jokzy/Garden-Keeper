bedb2b6fc30d40b67dd879c6a85d5f6a2e9afc8a
Camille Marquis <camimarquis@gmail.com>
2024-03-22 11:01:32 -0400

    ajouter model plante et essaie de GET dans views

--- back_end/gk_projet/api/serializers.py
+++ back_end/gk_projet/api/serializers.py
@@ -1,9 +1,9 @@
 # What does a serializer do? It converts instances of our items from objects into datatypes the respoonse object can understand
 
 from rest_framework import serializers
-from gk_app.models import Item
+from gk_app.models import Plante
 
-class ItemSerializer(serializers.ModelSerializer):
+class PlanteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
+        model = Plante
         fields ='__all__'
--- back_end/gk_projet/api/views.py
+++ back_end/gk_projet/api/views.py
@@ -1,17 +1,31 @@
+import requests
+import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from gk_app.models import Item
-from .serializers import ItemSerializer
+from gk_app.models import Plante
+from .serializers import PlanteSerializer
+from django.http import HttpResponse
+
+api_key = "sk-eKNW65d813327065e4323" #perenual
+
+# @api_view(['GET'])
+# def getData(request):
+#     items = Plante.objects.all()
+#     serializer = ItemSerializer(items, many=True)
+#     return Response(serializer.data) # This outputs json data
 
 @api_view(['GET'])
-def getData(request):
-    items = Item.objects.all()
-    serializer = ItemSerializer(items, many=True)
-    return Response(serializer.data) # This outputs json data
+def getData(query):
+    perenual_url = f"https://perenual.com/api/species-list?key={api_key}&q={query}"
+    r= requests.get(perenual_url)
+    info= r.json()
+    temp=info['data'][0]['watering']
+    return HttpResponse(temp)
+
 
 @api_view(['POST'])
 def addItem(request):
-    serializer = ItemSerializer(data=request.data)
+    serializer = PlanteSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data) # This outputs json data
