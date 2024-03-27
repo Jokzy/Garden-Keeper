3bf786dbb2138efdb5f2ae42161c396e2713a9f5
Jacob Boucher <boucja2@gmail.com>
2024-03-27 15:25:01 -0400

    2024/03/27
    J'ai mis l'image pour le settings/main menu screen
    La tab bar est transparente, j'ai mis en commentaires how I did it

--- front_end/Garden_Keeper/App.js
+++ front_end/Garden_Keeper/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import {View, TextInput, Button, Text, StyleSheet, ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
 import ScreenEnc from "./Encyclopédie";
@@ -18,22 +18,32 @@ export default function App() {
   const topTab = createMaterialTopTabNavigator()
   function SettingsScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Settings!</Text>
-        </View>
+        <ImageBackground source={require("./assets/background1.png")} resizeMode={"cover"} style={styles.image}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+          </View>
+        </ImageBackground>
     );
   }
 
 return (
-    <NavigationContainer theme={navigationTheme}>
-      <topTab.Navigator
-          screenOptions={ {
-            tabBarStyle: {
-              position: 'absolute',
-              borderTopLeftRadius: 12,
-              borderTopRightRadius: 12,
-              backgroundColor: 'transparent'}}}>
-        <topTab.Screen name="Menu" component={SettingsScreen}/>
+    <NavigationContainer  theme={navigationTheme}>
+      <topTab.Navigator screenOptions={ {
+        tabBarStyle: {
+          position: 'absolute',
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+          top:30, //Top, bottom, right, left, sont la position arbitraire de la tab bar et permettent de la voir.
+          bottom: 695,
+          right:0,
+          left:0,
+
+          elevation: 0, // Elevation 0 fait qu'il y a pas de shadow en dessous de la tab bar
+          //paddingTop: StatusBar.height, I tried to not make the status bar and top bar overlap. Unsuccessfully
+          backgroundColor: 'transparent'}}}
+        >
+
+        <topTab.Screen  name="Menu" component={SettingsScreen}/>
         <topTab.Screen name="Encyclopédie" component={ScreenEnc}/>
         <topTab.Screen name="Jardin" component={ScreenJar}/>
         <topTab.Screen name="Amis" component={ScreenAmi}/>
@@ -63,5 +73,17 @@ return (
         marginTop: 20,
         fontSize: 16,
       },
+          image: {
+            flex: 1,
+            justifyContent: 'center',
+
+
+
+          },
+          textScreen:{
+          color: "darkgreen",
+            fontWeight: "bold",
+            fontFamily: "Adlamdisplay"
+          }
       });
 
