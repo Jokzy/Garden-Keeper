994f00575c13488ae01c4501b7190aa0d5feaf5a
Sonia Ly <lysoksonia@gmail.com>
2024-05-09 12:29:32 -0400

    Merged backend to frontend

--- Backend/gk_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Plante
-
-
-class PlanteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Plante
-        fields = ('id', 'nom', 'nom_scientifique')
